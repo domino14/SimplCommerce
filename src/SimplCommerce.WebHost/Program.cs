2fcb1987277273a5d8e4cd389106414474b8387a
César Del Solar <delsolar@gmail.com>
2024-05-31 10:54:04 -0400

    use nuget

--- src/SimplCommerce.WebHost/Program.cs
+++ src/SimplCommerce.WebHost/Program.cs
@@ -25,14 +25,6 @@ using OpenTelemetry;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 using System.Diagnostics;
-using Microsoft.AspNetCore.Http;
-using System.Threading.Tasks;
-
-using System.Net.Http;
-using System.Collections.Generic;
-using System.IO;
-using System.Text.Json;
-using System.Text;
 
 var serviceName = "SimplCommerce.CesarsFork";
 var serviceVersion = "1.0.0";
@@ -40,12 +32,17 @@ var serviceVersion = "1.0.0";
 var builder = WebApplication.CreateBuilder(args);
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
+#if DEBUG
+Console.WriteLine("Build Configuration: Debug");
+#else
+Console.WriteLine("Build Configuration: Release");
+#endif
+
+
 ConfigureService();
 var app = builder.Build();
 Configure();
 app.Run();
-
-
 
 
 void ConfigureService()
@@ -130,7 +127,12 @@ void Configure()
         );
         app.UseHsts();
     }
-    app.UseMiddleware<RequestResponseLoggingMiddleware>();
+    app.UseMiddleware<CodeComet.CaptureMiddleware>(
+        "cc-e23d00ac-d60bd829d922cd9f4c068b218a431339",  // api key
+        "a63069c8-ffc0-4d87-a30f-f27b4b212d71",   // project ID
+        false,  // send all traffic, defaults to false.
+        "http://localhost/api/trafficconsumer.TrafficService/IngestTrafficLog" // defaults to the prod URL
+    );
 
     app.UseWhen(
         context => !context.Request.Path.StartsWithSegments("/api", StringComparison.OrdinalIgnoreCase),
@@ -209,161 +211,3 @@ public static class Telemetry
 //     }
 // }
 
-
-
-public class RequestResponseLoggingMiddleware
-{
-    private readonly RequestDelegate _next;
-    private readonly HttpClient _httpClient;
-    private const string ApiKey = "cc-e23d00ac-d60bd829d922cd9f4c068b218a431339";
-    private const string ProjectID = "a63069c8-ffc0-4d87-a30f-f27b4b212d71";
-    private const string ExternalServerUrl = "http://localhost/api/trafficconsumer.TrafficService/IngestTrafficLog";
-
-    private const string RFC3339Fmt = "yyyy-MM-ddTHH:mm:ss.fffK";
-
-    public RequestResponseLoggingMiddleware(RequestDelegate next)
-    {
-        _next = next;
-        _httpClient = new HttpClient();
-        _httpClient.DefaultRequestHeaders.Add("Api-Key", ApiKey);
-    }
-
-    public async Task InvokeAsync(HttpContext context)
-    {
-        var staticExtensions = new HashSet<string> { ".js", ".css", ".png", ".jpg", ".jpeg", ".gif", ".svg", ".ico", ".woff2" };
-        if (staticExtensions.Any(ext => context.Request.Path.Value.EndsWith(ext, StringComparison.OrdinalIgnoreCase)))
-        {
-            // It's a static file request, so just continue to the next middleware
-            await _next(context);
-        }
-        else
-        {
-            var requestData = new Dictionary<string, object>();
-            var originalBodyStream = context.Response.Body;
-            // Capture the request timestamp
-            var requestTimestamp = DateTimeOffset.UtcNow;
-            requestData["request_time"] = requestTimestamp.ToString(RFC3339Fmt);
-            requestData["project_id"] = ProjectID;
-            using (var responseBody = new MemoryStream())
-            {
-                context.Response.Body = responseBody;
-
-                try
-                {
-                    // Collect request and response details
-                    await CollectRequestData(context, requestData);
-                    await _next(context);
-
-                    context.Response.Body.Seek(0, SeekOrigin.Begin);
-                    var responseBodyText = await new StreamReader(context.Response.Body).ReadToEndAsync();
-                    context.Response.Body.Seek(0, SeekOrigin.Begin);
-
-                    await responseBody.CopyToAsync(originalBodyStream);
-                    // collect response data
-                    // Capture the response timestamp
-                    var responseTimestamp = DateTimeOffset.UtcNow;
-                    requestData["response_time"] = responseTimestamp.ToString(RFC3339Fmt);
-
-                    requestData["raw_response"] = responseBodyText;
-                    requestData["status_code"] = context.Response.StatusCode;
-                }
-                catch (Exception ex)
-                {
-                    if (context.Response.StatusCode == 200 || context.Response.StatusCode == 0)
-                    {
-                        context.Response.StatusCode = 500;
-                    }
-                    requestData["status_code"] = context.Response.StatusCode;
-                    HandleException(context, ex, requestData);
-                    throw;
-                }
-                finally
-                {
-                    context.Response.Body = originalBodyStream;
-                    // Send data to external server
-                    await SendDataToExternalServer(requestData);
-                }
-            }
-        }
-    }
-
-    private string GetRawHeaders(IHeaderDictionary headers)
-    {
-        var stringBuilder = new StringBuilder();
-
-        foreach (var header in headers)
-        {
-            foreach (var value in header.Value)
-            {
-                stringBuilder.AppendLine($"{header.Key}: {value}");
-            }
-        }
-
-        return stringBuilder.ToString();
-    }
-
-    private async Task CollectRequestData(HttpContext context, Dictionary<string, object> data)
-    {
-        var request = context.Request;
-
-        // Ensure the request body can be read multiple times
-        request.EnableBuffering();
-
-        // Read the request body
-        var body = string.Empty;
-        var requestBodyStream = new MemoryStream();
-        await request.Body.CopyToAsync(requestBodyStream);
-        requestBodyStream.Seek(0, SeekOrigin.Begin);
-
-        using (var reader = new StreamReader(requestBodyStream))
-        {
-            body = await reader.ReadToEndAsync();
-            // Reset the stream so that it can be read again later
-            request.Body.Seek(0, SeekOrigin.Begin);
-        }
-
-
-        data["method"] = request.Method;
-        data["path"] = GetRequestPath(request);
-        data["query_string"] = request.QueryString.ToString();
-        data["request_headers"] = GetRawHeaders(request.Headers);
-        data["raw_request"] = body;
-    }
-
-    private string GetRequestPath(HttpRequest request)
-    {
-        return request.Path.HasValue ? request.Path.ToString() : string.Empty;
-    }
-
-    private void HandleException(HttpContext context, Exception exception, Dictionary<string, object> data)
-    {
-        data["exception_message"] = exception.Message;
-        data["traceback"] = exception.StackTrace;
-    }
-
-    private async Task SendDataToExternalServer(Dictionary<string, object> data)
-    {
-        try
-        {
-            var wrapper = new Dictionary<string, object>();
-            wrapper["log"] = data;
-            var content = new StringContent(JsonSerializer.Serialize(wrapper), Encoding.UTF8, "application/json");
-            var response = await _httpClient.PostAsync(ExternalServerUrl, content);
-            Console.WriteLine("-----");
-            Console.WriteLine(JsonSerializer.Serialize(wrapper));
-            Console.WriteLine("-----");
-
-            // Check the response
-            if (!response.IsSuccessStatusCode)
-            {
-                var responseContent = await response.Content.ReadAsStringAsync();
-                Console.WriteLine($"Failed to log data: {response.StatusCode} - {responseContent}");
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error sending data to external server: {ex.Message}");
-        }
-
-    }
-}
